685954500040d7d2f53b583a6f52fa46a48b354f
brad-bb-bell <brad.bell5@montana.edu>
2022-12-30 10:12:34 -0700

    adds the skeleton login container

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="">
-    <Container>hi</Container>
+    <Container>
+      <h1 class="text-center text-xl tracking-widest">🤸 🏋️ 🧘 Simple Activity Tracker 🧘 🏋️ 🤸</h1>
+    </Container>
+    <Container v-if="!isLoggedIn">
+      <div class="text-center">
+        <button class="mr-14" @click="">Login</button>
+        <button class="ml-14" @click="">Signup</button>
+      </div>
+      <!-- <form @submit="login()">
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <input type="email" placeholder="Email" v-model="loginParams.email" />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" v-model="loginParams.password" />
+        </div>
+        <input type="submit" value="Login" />
+      </form> -->
+    </Container>
   </div>
 </template>
 <script>
+import axios from "axios";
 import Container from "./components/ContainerBase.vue";
 export default {
   components: {
     Container,
+  },
+  data() {
+    return {
+      errors: [],
+      loginParams: {},
+      isLoggedIn: false,
+    };
+  },
+  methods: {
+    login() {
+      console.log("login");
+    },
   },
 };
 </script>
