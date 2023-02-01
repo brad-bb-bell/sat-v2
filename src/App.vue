ef000434892c7ae50fb60f2aa15e4e400c16fc63
brad-bb-bell <brad.bell5@montana.edu>
2023-02-01 14:23:09 -0700

    adds addActivity and deleteActivity methods

--- src/App.vue
+++ src/App.vue
@@ -1,167 +1,175 @@
 <template>
-  <div class="">
-    <Container>
-      <h1 class="text-center text-xl tracking-widest">🤸 🏋️ 🧘 Simple Activity Tracker 🧘 🏋️ 🤸</h1>
-    </Container>
+  <Container>
+    <h1 class="text-center text-xl tracking-widest">🤸 🏋️ 🧘 Simple Activity Tracker 🧘 🏋️ 🤸</h1>
+  </Container>
 
-    <!-- Login or Signup -->
-    <Container v-if="!isLoggedIn && !showLogin && !showSignup">
+  <!-- Login or Signup -->
+  <Container v-if="!isLoggedIn && !showLogin && !showSignup">
+    <div class="grid grid-cols-2">
+      <button class="mx-auto" @click="showLogin = true">Login</button>
+      <button class="mx-auto" @click="showSignup = true">Signup</button>
+    </div>
+  </Container>
+
+  <!-- Signup -->
+  <Container v-if="showSignup">
+    <form @submit.prevent="signup()">
+      <div class="text-center">
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </div>
       <div class="grid grid-cols-2">
-        <button class="mx-auto" @click="showLogin = true">Login</button>
-        <button class="mx-auto" @click="showSignup = true">Signup</button>
+        <div class="flex flex-col gap-2 py-2 items-center">
+          <div>
+            <input type="username" placeholder="Username" v-model="signupParams.name" class="text-black rounded px-1" />
+          </div>
+          <div>
+            <input
+              type="email"
+              placeholder="Email address"
+              v-model="signupParams.email"
+              class="text-black rounded px-1"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              v-model="signupParams.password"
+              class="text-black rounded px-1"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password confirmation"
+              v-model="signupParams.password_confirmation"
+              class="text-black rounded px-1"
+            />
+          </div>
+        </div>
+        <div class="text-center my-auto">
+          <h2>New User Signup</h2>
+          <button type="submit" class="w-[100px] border text-black font-medium bg-green-400 text-sm rounded my-1">
+            Submit
+          </button>
+          <p>
+            <button
+              @click.prevent="
+                this.showSignup = false;
+                this.errors = [];
+              "
+              class="w-[100px] border text-black font-medium bg-gray-400 text-sm rounded my-1"
+            >
+              Back
+            </button>
+          </p>
+        </div>
+      </div>
+    </form>
+  </Container>
+
+  <!-- Login -->
+  <Container v-if="showLogin">
+    <form @submit.prevent="login()">
+      <div class="text-center">
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </div>
+      <div class="grid grid-cols-2">
+        <div class="flex flex-col gap-2 my-2 items-center">
+          <div>
+            <input type="email" placeholder="Email" v-model="loginParams.email" class="text-black rounded px-1" />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              v-model="loginParams.password"
+              class="text-black rounded px-1"
+            />
+          </div>
+        </div>
+
+        <div class="text-center my-auto">
+          <button type="submit" class="border text-black font-medium bg-green-400 px-8 text-sm mx-10 my-1 rounded">
+            Login
+          </button>
+          <p>
+            <button
+              @click.prevent="
+                this.showLogin = false;
+                this.errors = [];
+              "
+              class="border text-black font-medium bg-gray-400 px-8 text-sm mx-10 my-1 rounded"
+            >
+              Back
+            </button>
+          </p>
+        </div>
+      </div>
+    </form>
+  </Container>
+
+  <!-- Successfully Logged In -->
+  <div v-if="isLoggedIn">
+    <Container class="grid grid-cols-2">
+      <div class="mx-auto">
+        {{ user.name }}
+      </div>
+      <div class="mx-auto">
+        <button @click="logout">Logout</button>
       </div>
     </Container>
 
-    <!-- Signup -->
-    <Container v-if="showSignup">
-      <form @submit.prevent="signup()">
-        <div class="text-center">
-          <ul>
-            <li v-for="error in errors" :key="error">{{ error }}</li>
-          </ul>
+    <!-- Activities -->
+    <Container class="py-2">
+      <div v-if="activities.length === 0" class="text-center mb-2">
+        Add an activity in the box below and press 'Enter'
+      </div>
+      <div v-for="activity in activities" :key="activity.id">
+        <div
+          @click="toggleSelect(activity.id)"
+          class="relative bg-gray-700 border-2 border-black my-1 pl-2 hover:bg-gray-600"
+          :class="[selectedActivities.includes(activity.id) === true ? 'border border-green-400' : '']"
+        >
+          {{ activity.name }}
+          <span class="absolute inset-y-0 right-2">
+            <i
+              @click="deleteActivity(activity)"
+              class="fa-solid fa-xmark text-red-400 hover:cursor-pointer hover:text-red-600"
+            ></i>
+          </span>
         </div>
-        <div class="grid grid-cols-2">
-          <div class="flex flex-col gap-2 py-2 items-center">
-            <div>
-              <input
-                type="username"
-                placeholder="Username"
-                v-model="signupParams.name"
-                class="text-black rounded px-1"
-              />
-            </div>
-            <div>
-              <input
-                type="email"
-                placeholder="Email address"
-                v-model="signupParams.email"
-                class="text-black rounded px-1"
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                v-model="signupParams.password"
-                class="text-black rounded px-1"
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password confirmation"
-                v-model="signupParams.password_confirmation"
-                class="text-black rounded px-1"
-              />
-            </div>
-          </div>
-          <div class="text-center my-auto">
-            <h2>New User Signup</h2>
-            <button type="submit" class="w-[100px] border text-black font-medium bg-green-400 text-sm rounded my-1">
-              Submit
-            </button>
-            <p>
-              <button
-                @click.prevent="
-                  this.showSignup = false;
-                  this.errors = [];
-                "
-                class="w-[100px] border text-black font-medium bg-gray-400 text-sm rounded my-1"
-              >
-                Back
-              </button>
-            </p>
-          </div>
-        </div>
-      </form>
+      </div>
+      <div class="">
+        <form @submit.prevent="addActivity(newActivityName)">
+          <input type="text" v-model="newActivityName" placeholder="Add Activity" class="w-[99%] m-1 pl-2 text-black" />
+        </form>
+      </div>
+      <Datepicker
+        v-model="calendarDate"
+        placeholder="Select Date"
+        :format="format"
+        :enableTimePicker="false"
+        @update:modelValue="calendarDate"
+        autoApply
+        dark
+      />
     </Container>
-
-    <!-- Login -->
-    <Container v-if="showLogin">
-      <form @submit.prevent="login()">
-        <div class="text-center">
-          <ul>
-            <li v-for="error in errors" :key="error">{{ error }}</li>
-          </ul>
-        </div>
-        <div class="grid grid-cols-2">
-          <div class="flex flex-col gap-2 my-2 items-center">
-            <div>
-              <input type="email" placeholder="Email" v-model="loginParams.email" class="text-black rounded px-1" />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                v-model="loginParams.password"
-                class="text-black rounded px-1"
-              />
-            </div>
-          </div>
-
-          <div class="text-center my-auto">
-            <button type="submit" class="border text-black font-medium bg-green-400 px-8 text-sm mx-10 my-1 rounded">
-              Login
-            </button>
-            <p>
-              <button
-                @click.prevent="
-                  this.showLogin = false;
-                  this.errors = [];
-                "
-                class="border text-black font-medium bg-gray-400 px-8 text-sm mx-10 my-1 rounded"
-              >
-                Back
-              </button>
-            </p>
-          </div>
-        </div>
-      </form>
-    </Container>
-
-    <!-- Successfully Logged In -->
-    <div v-if="isLoggedIn">
-      <Container class="grid grid-cols-2">
-        <div class="mx-auto">
-          {{ user.name }}
-        </div>
-        <div class="mx-auto">
-          <button @click="logout">Logout</button>
-        </div>
-      </Container>
-
-      <!-- Activities -->
-      <Container class="py-2">
-        <div v-for="activity in activities" :key="activity.id">
-          <div
-            @click="toggleSelect(activity.id)"
-            class="relative bg-gray-700 border-2 border-black my-1 pl-2"
-            :class="[selectedActivities.includes(activity.id) === true ? 'border border-green-400' : '']"
-          >
-            {{ activity.name }}
-            <span class="absolute inset-y-0 right-2">
-              <i
-                @click="deleteActivity(activity.id)"
-                class="fa-solid fa-xmark text-red-400 hover:cursor-pointer hover:text-red-600"
-              ></i>
-            </span>
-          </div>
-        </div>
-        <div class="">
-          <form @submit.prevent="addActivity()">
-            <input type="text" v-model="newActivity" placeholder="Add Activity" class="w-[99%] m-1 pl-2 text-black" />
-          </form>
-        </div>
-      </Container>
-    </div>
   </div>
 </template>
 <script>
 import axios from "axios";
 import Container from "./components/ContainerBase.vue";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 export default {
   components: {
     Container,
+    Datepicker,
   },
   data() {
     return {
@@ -174,7 +182,8 @@ export default {
       user: {},
       activities: [],
       selectedActivities: [],
-      newActivity: "",
+      newActivityName: "",
+      calendarDate: "",
     };
   },
   methods: {
@@ -221,12 +230,51 @@ export default {
           this.signupParams = {};
         });
     },
-    deleteActivity(id) {
-      console.log("Activity with ID " + id + " will be deleted");
+
+    deleteActivity(activity) {
+      if (confirm("Are you sure you want to permanently remove this activity and all associated Did Its?")) {
+        // var deleteName = activity.name;
+        var deleteActivityId = activity.id;
+        // console.log("didIts length", this.fullDidIts.length);
+        // this.fullDidIts.forEach(
+        //   (didIt) => {
+        //     console.log("forEach");
+        //     if (didIt.name === deleteName) {
+        //       console.log("delete me", didIt);
+        //       var deleteId = didIt.id;
+        //       axios.delete("/did_its/" + deleteId + ".json").then((response) => {
+        //         console.log("Success,", response.data);
+        //         this.didIts = this.didIts.filter((didIt) => didIt.id != deleteId);
+        //       });
+        //     }
+        //   },
+        axios
+          .delete("/activities/" + deleteActivityId + ".json")
+          .then((response) => {
+            console.log("Success,", response.data);
+            this.activities = this.activities.filter((activity) => activity.id != deleteActivityId);
+          })
+          .catch((error) => {
+            console.log(error.response.data.errors);
+          });
+        // );
+      }
     },
-    addActivity() {
-      console.log("Add " + this.newActivity);
-      this.newActivity = "";
+    addActivity(name) {
+      const newActivity = {
+        user_id: localStorage.user_id,
+        name: name,
+      };
+      axios
+        .post("/activities.json", newActivity)
+        .then((response) => {
+          console.log("Successfully added new activity", response.data);
+          this.activities.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+      this.newActivityName = "";
     },
     toggleSelect(id) {
       if (this.selectedActivities.includes(id) === false) {
@@ -237,6 +285,19 @@ export default {
           this.selectedActivities.splice(index, 1);
         }
       }
+    },
+    format() {
+      const day = this.date.getDate();
+      const month = this.date.getMonth() + 1;
+      const year = this.date.getFullYear();
+
+      return `${month}/${day}/${year}`;
+    },
+    showDate(date) {
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      this.calendarDate = `${year}-${month}-${day}`;
     },
   },
   created() {
@@ -249,5 +310,21 @@ export default {
       });
     }
   },
+  // setup() {
+  //   const date = ref(new Date());
+
+  //   const format = (date) => {
+  //     const day = date.getDate();
+  //     const month = date.getMonth() + 1;
+  //     const year = date.getFullYear();
+
+  //     return `${month}/${day}/${year}`;
+  //   };
+
+  //   return {
+  //     date,
+  //     format,
+  //   };
+  // },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import axios from "axios";
 import "./input.css";
 
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
@@ -10,3 +13,4 @@ if (jwt) {
 }
 
 createApp(App).mount("#app");
+App.component("Date-picker", Datepicker);
