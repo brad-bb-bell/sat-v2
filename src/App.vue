72ea31117d513e3df46fd46edd9c82c3c4a4b779
brad-bb-bell <brad.bell5@montana.edu>
2023-02-28 15:34:14 -0700

    implementing dropdown from headless ui

--- src/App.vue
+++ src/App.vue
@@ -204,6 +204,46 @@
         This year: {{ favoriteCount.year }}x
         <div v-if="this.favoriteCount.lastYear > 0">Last year: {{ favoriteCount.lastYear }}x</div>
       </div>
+
+      <div class="text-center">
+        <Menu as="div" class="relative inline-block text-left">
+          <div>
+            <MenuButton
+              class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+            >
+              {{ favoriteActivity.name }}
+              <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            </MenuButton>
+          </div>
+
+          <transition
+            enter-active-class="transition ease-out duration-100"
+            enter-from-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75"
+            leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
+          >
+            <MenuItems
+              class="absolute translate-x-[-35%] z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+              <div v-for="activity in activities">
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <button
+                      class="mx-auto"
+                      :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']"
+                      @click.prevent="changeFavorite(activity)"
+                    >
+                      {{ activity.name }}
+                    </button>
+                  </MenuItem>
+                </div>
+              </div>
+            </MenuItems>
+          </transition>
+        </Menu>
+      </div>
     </Container>
 
     <!-- All Activities -->
@@ -257,10 +297,17 @@ import axios from "axios";
 import Container from "./components/ContainerBase.vue";
 import Datepicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
 export default {
   components: {
     Container,
     Datepicker,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    ChevronDownIcon,
   },
   data() {
     return {
@@ -644,6 +691,9 @@ export default {
       var temp = this.user.did_its;
       this.sortByDate(temp);
       this.didIts = temp.reverse().slice(0, this.didItsNumber);
+    },
+    changeFavorite(activity) {
+      console.log(activity.name);
     },
   },
   created() {
