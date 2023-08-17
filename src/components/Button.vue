0d047cebf2f6f8ca09d30fddd13f5a1dbd5f68f6
brad-bb-bell <brad.bell5@montana.edu>
2023-08-17 16:12:12 -0600

    creates button component

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="rounded border px-3 py-1.5" role="button">
+    <slot class="select-none"></slot>
+  </button>
+</template>
+<script>
+  export default {
+    props: {
+      type: {
+        type: String,
+        default: 'null'
+      }
+    }
+  }
+</script>
